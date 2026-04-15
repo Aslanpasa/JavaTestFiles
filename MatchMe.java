f155aec24014bfbab12c7c16924aee5937564793
Aslanpasa <ferociouslionimportantoa@gmail.com>
2026-04-15 13:22:42 +1000

    Create MatchMe.java

---
+++ MatchMe.java
@@ -0,0 +1,26 @@
+public class MatchMe {
+    public static void main(String[] args) {
+        /*
+        PROMT the user for the range of numbers.
+        CREATE collection to hold 2 of each number.
+        POPULATE the collection with all the numbers including duplicates in ascending order
+        PRINT the table of numbers in the original order with a maximum of 5 numbers per row.
+        SET counter to maximum index of collection to track how many numbers have not been used.
+        SEED a randomiser for testing purposes (use the Random class for this).
+        ITERATE over the collection until all numbers have been used
+            RETRIEVE a random number (to be used as an index for retrieving a value 
+            from the original collection.)
+            IF the number has not been used before (check if the value is equal to zero,  
+            meaning it has already been used)
+                PRINT the number
+                SET the original number to zero to mark as used
+                IF a new line is required.
+                PRINT new line
+                RESET new line required counter
+            Decrement unusedNumbers
+        */
+        System.out.println("Hello World");
+    }
+
+
+}
